c8b7fea7175dfc7309b8bc99f62fe3fd08ffa783
chris <chris.fhy@gmail.com>
2020-03-08 00:11:06 +0800

    [new feature]: Support reversing
    Connect I1 I2 I3 I4 of L298N directly to GPIO of Raspberry Pi.
    
    Signed-off-by: chris <chris.fhy@gmail.com>

--- pwm_control_motor.py
+++ pwm_control_motor.py
@@ -1,12 +1,19 @@
 #!/usr/bin/python
 
 from Raspi_PWM_Servo_Driver import PWM
+import RPi.GPIO as GPIO
 import time
 import utils
 
 
 LEFT_WHEEL_CHANNEL = 8
 RIGHT_WHEEL_CHANNEL = 7
+
+L298N_I1_PIN = 4
+L298N_I2_PIN = 17
+
+L298N_I3_PIN = 23
+L298N_I4_PIN = 24
 
 
 class PWMControlMotor:
@@ -16,6 +23,17 @@ class PWMControlMotor:
     But it seems the min pulse width must be set to 800 to enable car to move.
     """
     def __init__(self, default_h_pos=0, default_v_pos=0):
+        GPIO.setmode(GPIO.BCM)
+        GPIO.setup(L298N_I1_PIN, GPIO.OUT)
+        GPIO.setup(L298N_I2_PIN, GPIO.OUT)
+        GPIO.setup(L298N_I3_PIN, GPIO.OUT)
+        GPIO.setup(L298N_I4_PIN, GPIO.OUT)
+
+        GPIO.output(L298N_I1_PIN, 0)
+        GPIO.output(L298N_I2_PIN, 0)
+        GPIO.output(L298N_I3_PIN, 0)
+        GPIO.output(L298N_I4_PIN, 0)
+
         self.pwm = PWM(0x6F, debug=True)
         self.pwm.setPWMFreq(50)
         self.current_horizontal_pos = default_h_pos
@@ -23,10 +41,44 @@ class PWMControlMotor:
         self.pwm.setPWM(LEFT_WHEEL_CHANNEL, 0, int(self.current_horizontal_pos))
         self.pwm.setPWM(RIGHT_WHEEL_CHANNEL, 0, int(self.current_vertical_pos))
 
+    def _set_direction(self, wheel, direction=0):
+        """
+        :param wheel: 0 for left wheel, 1 for right wheel
+        :param direction: 0 for go ahead, 1 for back
+        :return:
+        """
+        if wheel == 0:
+            if direction == 0:
+                GPIO.output(L298N_I1_PIN, 1)
+                GPIO.output(L298N_I2_PIN, 0)
+                print('set I1 to 1 and I2 to 0')
+            elif direction == 1:
+                GPIO.output(L298N_I1_PIN, 0)
+                GPIO.output(L298N_I2_PIN, 1)
+                print('set I1 to 0 and I2 to 1')
+            else:
+                print("unknown direction: {}".format(direction))
+        elif wheel == 1:
+            if direction == 0:
+                GPIO.output(L298N_I3_PIN, 0)
+                GPIO.output(L298N_I4_PIN, 1)
+                print('set I3 to 0 and I4 to 1')
+            elif direction == 1:
+                GPIO.output(L298N_I3_PIN, 1)
+                GPIO.output(L298N_I4_PIN, 0)
+                print('set I3 to 1 and I4 to 0')
+            else:
+                print('unknown direction: {}'.format(direction))
+        else:
+            print('unknown wheel {}'.format(wheel))
+
     def set_pulse_width(self, wheel_channel, pulse_width):
         self.pwm.setPWM(wheel_channel, 0, pulse_width)
 
     def go_forward(self, seconds=0):
+        self._set_direction(0, 0)
+        self._set_direction(1, 0)
+
         self.set_pulse_width(LEFT_WHEEL_CHANNEL, 4000)
         self.set_pulse_width(RIGHT_WHEEL_CHANNEL, 4000)
         if seconds > 0:
@@ -34,34 +86,49 @@ class PWMControlMotor:
             self.stop()
 
     def turn_left(self, seconds=0):
-        self.set_pulse_width(LEFT_WHEEL_CHANNEL, 0)
-        self.set_pulse_width(RIGHT_WHEEL_CHANNEL, 3000)
+        self._set_direction(0, 1)
+        self._set_direction(1, 0)
+
+        self.set_pulse_width(LEFT_WHEEL_CHANNEL, 4000)
+        self.set_pulse_width(RIGHT_WHEEL_CHANNEL, 4000)
         if seconds > 0:
             time.sleep(seconds)
             self.stop()
 
     def turn_right(self, seconds=0):
-        self.set_pulse_width(RIGHT_WHEEL_CHANNEL, 0)
-        self.set_pulse_width(LEFT_WHEEL_CHANNEL, 3000)
+        self._set_direction(0, 0)
+        self._set_direction(1, 1)
+        self.set_pulse_width(RIGHT_WHEEL_CHANNEL, 4000)
+        self.set_pulse_width(LEFT_WHEEL_CHANNEL, 4000)
         if seconds > 0:
             time.sleep(seconds)
             self.stop()
 
-    def stop(self, seconds=0):
+    def stop(self):
+        self._set_direction(0, 1)
+        self._set_direction(1, 1)
         self.set_pulse_width(RIGHT_WHEEL_CHANNEL, 0)
         self.set_pulse_width(LEFT_WHEEL_CHANNEL, 0)
+
+    def back_up(self, seconds=0):
+        self._set_direction(0, 1)
+        self._set_direction(1, 1)
+        self.set_pulse_width(RIGHT_WHEEL_CHANNEL, 4000)
+        self.set_pulse_width(LEFT_WHEEL_CHANNEL, 4000)
         if seconds > 0:
             time.sleep(seconds)
             self.stop()
 
+    def clean_gpio(self):
+        GPIO.cleanup()
 
 if __name__ == '__main__':
     pwm_control_motor = PWMControlMotor()
     while True:
         if utils.isPython3():
-            direction = input("please input 'l' or 'r' or 'g' or 's':")
+            direction = input("please input 'l' or 'r' or 'g' or 's' or 'b':")
         else:
-            direction = raw_input("please input 'l' or 'r' or 'g' or 's':")
+            direction = raw_input("please input 'l' or 'r' or 'g' or 's' or 'b':")
 
         if direction == 'l':
             pwm_control_motor.turn_left(2)
@@ -70,6 +137,12 @@ if __name__ == '__main__':
         elif direction == 'g':
             pwm_control_motor.go_forward(2)
         elif direction == 's':
-            pwm_control_motor.stop(2)
+            pwm_control_motor.stop()
+        elif direction == 'b':
+            pwm_control_motor.back_up(2)
+        elif direction == 'quit':
+            break
         else:
             print("don't support this command!")
+
+    pwm_control_motor.clean_gpio()
--- remoteControl.py
+++ remoteControl.py
@@ -29,7 +29,7 @@ if utils.isPython3():
 	go_forward_key = b'ud'
 	turn_left_key = b'ld'
 	turn_right_key = b'rd'
-	stop_key = b'dd'
+	back_up_key = b'dd'
 
 	camera_up_key = b'udc'
 	camera_left_key = b'ldc'
@@ -39,7 +39,7 @@ else:
 	go_forward_key = 'ud'
 	turn_left_key = 'ld'
 	turn_right_key = 'rd'
-	stop_key = 'dd'
+	back_up_key = 'dd'
 
 	camera_up_key = 'udc'
 	camera_left_key = 'ldc'
@@ -63,13 +63,13 @@ while True:
 			data_array = data.split('\n')
 
 		if go_forward_key in data_array:
-			rr.go_forward(0.075)
+			rr.go_forward(0.2)
 		elif turn_left_key in data_array:
-			rr.turn_left(0.075)
-		elif stop_key in data_array:
-			rr.stop()
+			rr.turn_left(0.1)
+		elif back_up_key in data_array:
+			rr.back_up(0.1)
 		elif turn_right_key in data_array:
-			rr.turn_right(0.075)
+			rr.turn_right(0.1)
 		elif camera_up_key in data_array:
 			camera_servo.update_pos(CAM_ANGLE_DECREASE, CAM_VERTICAL_DIR, 5)
 		elif camera_down_key in data_array:
